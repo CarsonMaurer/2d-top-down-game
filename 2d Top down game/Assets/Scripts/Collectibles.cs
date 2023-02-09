4bc93265864e52db87a11d58e03167ec69b93d8e
Carson Maurer <carsonjmaurer@gmail.com>
2023-02-09 10:59:26 -0800

    fixed collectable script

--- 2d Top down game/Assets/Scripts/Collectibles.cs
+++ 2d Top down game/Assets/Scripts/Collectibles.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
+    [SerializeField] private int _value = 0;
     // Start is called before the first frame update
     void Start()
     {
